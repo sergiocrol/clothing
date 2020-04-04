56cb16fa22d3c8cc11c962a72c17b64e60f501fa
Sergio Cordero <sergio.crol@gmail.com>
2020-04-04 21:08:27 +0200

    Add checkout page, checkout item compoenent, and update cart to use selectors

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignPage from './pages/signpage/signpage.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 import { setCurrentUser } from './redux/user/user.actions';
+import { toggleCartHidden } from './redux/cart/cart.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCartHidden } from './redux/cart/cart.selectors';
 
 class App extends Component {
   unsubscribeFromAuth = null;
@@ -42,28 +47,34 @@ class App extends Component {
   }
 
   render() {
+    const { hidden, isCartHidden } = this.props;
     return (
-      <div>
+      <>
         <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
-          <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/' /> : <SignPage />} />
-        </Switch>
-      </div>
+        <div onClick={isCartHidden ? null : hidden}>
+          <Switch >
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route exact path='/signin' render={() => this.props.currentUser ? <Redirect to='/' /> : <SignPage />} />
+            <Route excat path='/checkout' component={CheckoutPage} />
+          </Switch>
+        </div>
+      </>
     )
   };
 }
 
 // We need our current user, so if the user is logged in we want not to get access to SignPage
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  isCartHidden: selectCartHidden
 });
 
 // Here we save in props a property called setCurrentUser which will run user.action.js; this property will be used above,
 // inside componentDidMount, passing a user.
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+  hidden: () => dispatch(toggleCartHidden())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,39 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 
-const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>SHOP</Link>
-      <Link className='option' to='/shop'>CONTACT</Link>
+const Header = ({ currentUser, hidden }) => {
+  console.log(currentUser);
+  return (
+    <div className='header'>
+      <Link className='logo-container' to='/'>
+        <Logo className='logo' />
+      </Link>
+      <div className='options'>
+        <Link className='option' to='/shop'>SHOP</Link>
+        <Link className='option' to='/shop'>CONTACT</Link>
+        {
+          currentUser ?
+            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+            :
+            <Link className='option' to='/signin'>SIGN IN</Link>
+        }
+        <CartIcon />
+      </div>
       {
-        currentUser ?
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-          :
-          <Link className='option' to='/signin'>SIGN IN</Link>
+        hidden ? null : <CartDropdown />
       }
-      <CartIcon />
     </div>
-    {
-      hidden ? null : <CartDropdown />
-    }
-  </div>
-);
+  )
+};
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
